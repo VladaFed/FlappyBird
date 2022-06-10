79d18894564e0641716300bce9b456114c992427
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-10 10:10:32 +0700

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import Model.Window;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //new Box();
+        Window window = new Window();
+        
+    }
+}
