588a2d41ed811b9fa312d46d0d6536733a6ea698
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-14 17:19:08 +0700

    Create Window.java

---
+++ src/main/java/Model/Window.java
@@ -0,0 +1,43 @@
+package Model;
+
+import Model.BirdPanel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class Window extends JFrame implements ActionListener {
+
+    BirdPanel birdP;
+
+    public Window() throws IOException {
+        initForm();
+    }
+
+    public void actionPerformed (ActionEvent e) {
+        if (birdP != null)
+            birdP.repaint();
+    }
+
+    private void initForm() {
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 800);
+        frame.setTitle("bird");
+
+        //new Box();
+        birdP = new BirdPanel();
+        frame.add(birdP);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        while (true) {
+            frame.repaint();
+        }
+    }
+
+
+
+}
