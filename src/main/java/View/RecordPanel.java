7c5e4c67db93e7d0e7e41c8f8065870594156afe
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-14 17:20:08 +0700

    Create RecordPanel.java

---
+++ src/main/java/View/RecordPanel.java
@@ -0,0 +1,36 @@
+package View;
+
+import java.util.ArrayList;
+import java.lang.Record;
+
+public class RecordPanel {
+
+    /*private final List<Note> table = new ArrayList<>();
+
+    //private RecordPanel() {}
+
+    public ArrayList<Note> getTable() {
+        return (ArrayList<Note>) table;
+    }
+
+    public void addTable(String namePlayer) { //name
+        for (int i = 0; i < table.size(); i++) {
+            //new Note("d", 3);
+            Note note = table.get(i);
+            if (note.name().equals(namePlayer)) {
+                if (note.score() >= score)
+                    break;
+                table.remove(i);
+                table.add(new Note(namePlayer, score));
+            }
+        }
+
+    }
+
+    public record Note(String name, Integer score) {
+        public String toString() {
+            return name + " " + score;
+        }
+    }*/
+
+}
