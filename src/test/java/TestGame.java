15c26e431c9de4314a4d2579456fe7b04bc2f480
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-25 10:07:23 +0700

    Update TestGame.java

--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -1,8 +1,16 @@
 import org.junit.jupiter.api.Test;
+import view.NewGame;
 
 public class TestGame {
-    @Test
-    void name() {
+    int PIPE_WIDTH = 70;
+    int PIPE_STARTX = 1400;
 
+    @Test
+    void CollisionEdgeBird() {
+        int pipePosition = PIPE_STARTX;
+        int birdPosition = pipePosition + PIPE_WIDTH/2;
+        //NewGame newGame = new NewGame();
+
+        //assertTrue();
     }
 }
