87bd27d8599c12306b947d9a437603ef451762ce
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-28 19:08:46 +0700

    Added tests

--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -1,16 +1,34 @@
-import org.junit.jupiter.api.Test;
+import main.GameConfiguration;
+import model.GameModel;
+import org.junit.Test;
 import view.NewGame;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class TestGame {
     int PIPE_WIDTH = 70;
     int PIPE_STARTX = 1400;
 
     @Test
-    void CollisionEdgeBird() {
-        int pipePosition = PIPE_STARTX;
-        int birdPosition = pipePosition + PIPE_WIDTH/2;
-        //NewGame newGame = new NewGame();
+    public void gameConfigurationParserTest() throws IOException {
+        GameConfiguration gameConfiguration = new GameConfiguration("config.properties");
+        gameConfiguration.readFromProperties();
 
-        //assertTrue();
+        assertEquals(PIPE_WIDTH, gameConfiguration.pipe_width);
+    }
+
+    @Test
+    public void gameOverModelTest() throws IOException {
+        GameConfiguration gameConfiguration = new GameConfiguration("config.properties");
+        gameConfiguration.readFromProperties();
+
+        GameModel model = new GameModel(gameConfiguration);
+
+        model.setGameOver();
+
+        assertEquals(true, model.isGameOver());
     }
 }
