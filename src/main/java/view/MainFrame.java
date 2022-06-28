1489f99fc2d56628380d73b98f85eacbab7f8d92
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-28 19:24:50 +0700

    Added isValid

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -2,6 +2,7 @@ package view;
 
 import controller.GamePresenter;
 import main.GameConfiguration;
+import main.GameConfigurationMenu;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,28 +11,31 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class MainFrame extends JFrame implements ActionListener {
-    private JLabel titleL, nameL, markL;
+    private JLabel titleL, nameL, markL, warningL;
     private JButton registrationB, aboutB, exitB, recordB, startGameB;
     public JTextField nameT = new JTextField(15);
     public String name;
     private final GameConfiguration gameConfiguration;
+    private final GameConfigurationMenu gameConfigurationMenu;
 
     GamePresenter gamePresenter;
 
     JFrame frame1 = new JFrame("Menu");
     JFrame frame2 = new JFrame("Registration");
+    JFrame about = new JFrame("About");
 
-
-    public MainFrame(GameConfiguration gConfiguration) {
+    public MainFrame(GameConfigurationMenu gameConfigurationMenu, GameConfiguration gameConfiguration) {
         super("FlappyBird");
-        gameConfiguration = gConfiguration;
-        frame1.setSize(gameConfiguration.menu_height, gameConfiguration.menu_width);
+        this.gameConfigurationMenu = gameConfigurationMenu;
+        this.gameConfiguration = gameConfiguration;
+        frame1.setSize(gameConfigurationMenu.menu_width, gameConfigurationMenu.menu_height);
         frame1.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         Container mainP = frame1.getContentPane();
         mainP.setLayout(null);
 
         titleL = new JLabel("Flappy Bird");
+        warningL = new JLabel("The high score table is available after the first game");
         registrationB = new JButton("Registration");
         aboutB = new JButton("About");
         recordB = new JButton("Table of record");
@@ -39,23 +43,33 @@ public class MainFrame extends JFrame implements ActionListener {
 
 
         mainP.add(titleL);
-        titleL.setFont(new Font("Chiller", Font.ITALIC, 50));
-        titleL.setBounds(150, 30, 200, 50);
+        titleL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.flappy_bird_size));
+        titleL.setBounds(gameConfigurationMenu.flappy_bird_x, gameConfigurationMenu.flappy_bird_y,
+                gameConfigurationMenu.flappy_bird_width, gameConfigurationMenu.flappy_bird_height);
+
+        mainP.add(warningL);
+        warningL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.warning_bird_size));
+        warningL.setBounds(gameConfigurationMenu.warning_x, gameConfigurationMenu.warning_y,
+                gameConfigurationMenu.warning_width, gameConfigurationMenu.warning_height);
 
         mainP.add(registrationB);
-        registrationB.setBounds(180, 90, 140, 20);
+        registrationB.setBounds(gameConfigurationMenu.registration_x, gameConfigurationMenu.registration_y,
+                gameConfigurationMenu.registration_width, gameConfigurationMenu.registration_height);
         registrationB.addActionListener(event -> panelStart());
 
         mainP.add(aboutB);
-        aboutB.setBounds(180, 110, 140, 20);
+        aboutB.setBounds(gameConfigurationMenu.about_x, gameConfigurationMenu.about_y,
+                gameConfigurationMenu.about_width, gameConfigurationMenu.about_height);
         aboutB.addActionListener(event -> printAbout());
 
         mainP.add(recordB);
-        recordB.setBounds(180, 130, 140, 20);
+        recordB.setBounds(gameConfigurationMenu.record_x, gameConfigurationMenu.record_y,
+                gameConfigurationMenu.record_width, gameConfigurationMenu.record_height);
         recordB.addActionListener(event -> tableGame());
 
         mainP.add(exitB);
-        exitB.setBounds(180, 150, 140, 20);
+        exitB.setBounds(gameConfigurationMenu.exit_x, gameConfigurationMenu.exit_y,
+                gameConfigurationMenu.exit_width, gameConfigurationMenu.exit_height);
         exitB.addActionListener(event -> System.exit(0));
 
         frame1.setLocationRelativeTo(null);
@@ -65,30 +79,34 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
     public void panelStart() {
-        frame2.setSize(400, 200);
+        frame2.setSize(gameConfigurationMenu.panel_start_width, gameConfigurationMenu.panel_start_height);
         frame2.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        Container nameP = frame2.getContentPane();
-        nameP.setLayout(null);
+        Container startP = frame2.getContentPane();
+        startP.setLayout(null);
 
         nameL = new JLabel("Registration");
         markL = new JLabel("Enter name: ");
         startGameB = new JButton("Start");
 
-        nameP.add(nameL);
-        nameL.setFont(new Font("Chiller", Font.ITALIC, 50));
-        nameL.setBounds(100, 30, 200, 50);
+        startP.add(nameL);
+        nameL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.registration_size));
+        nameL.setBounds(gameConfigurationMenu.registration_label_x, gameConfigurationMenu.registration_label_y,
+                gameConfigurationMenu.registration_label_width, gameConfigurationMenu.registration_label_height);
 
-        nameP.add(markL);
-        markL.setFont(new Font("Chiller", Font.ITALIC, 20));
-        markL.setBounds(30, 70, 200, 50);
+        startP.add(markL);
+        markL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.enter_name_size));
+        markL.setBounds(gameConfigurationMenu.enter_name_x, gameConfigurationMenu.enter_name_y,
+                gameConfigurationMenu.enter_name_width, gameConfigurationMenu.enter_name_height);
 
-        nameP.add(startGameB);
-        startGameB.setBounds(120, 110, 140, 20);
+        startP.add(startGameB);
+        startGameB.setBounds(gameConfigurationMenu.start_x, gameConfigurationMenu.start_y,
+                gameConfigurationMenu.start_width, gameConfigurationMenu.start_height);
         startGameB.addActionListener(event -> {
             try {
                 frame2.dispose();
                 name = nameT.getText();
+                if (name.length() > 15 || !isValid(name)) return;
                 gamePresenter = new GamePresenter(gameConfiguration);
                 gamePresenter.newGame(name);
             } catch (IOException e) {
@@ -96,8 +114,9 @@ public class MainFrame extends JFrame implements ActionListener {
             }
         });
 
-        nameP.add(nameT);
-        nameT.setBounds(120, 87, 140, 20);
+        startP.add(nameT);
+        nameT.setBounds(gameConfigurationMenu.name_x, gameConfigurationMenu.name_y,
+                gameConfigurationMenu.name_width, gameConfigurationMenu.name_height);
         nameT.addActionListener(this);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -107,9 +126,22 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
     public void printAbout() {
-        // CR: make the same way as records / registation
-        AboutPanel aboutPanel = new AboutPanel();
-        //aboutPanel.repaint();
+        About panel = new About();
+        about.getContentPane().add(panel);
+        about.setSize(gameConfigurationMenu.about_panel_width, gameConfigurationMenu.about_panel_height);
+        about.setLocationRelativeTo(null);
+        about.setVisible(true);
+
+    }
+
+    private boolean isValid(String name) {
+        if(name.length() == 0) return false;
+        for(int i = 0; i < name.length(); i++) {
+            if(name.charAt(i) == ' ') {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void tableGame() {
