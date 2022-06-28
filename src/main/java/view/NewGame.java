bfbbceb9e7bbceece10b91b4afa8da896ef88005
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-28 19:24:48 +0700

    Added setGameState

--- src/main/java/view/NewGame.java
+++ src/main/java/view/NewGame.java
@@ -14,15 +14,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class NewGame extends JPanel {
-    // CR: private static final
-    private Image back;
-    private Image bird;
-    private Image pipe1;
-    private Image pipe2;
+    private static final Image back = new ImageIcon("src/main/resources/pictures/back7.png").getImage();
+    private static final Image bird = new ImageIcon("src/main/resources/pictures/bird7.png").getImage();
+    private static final Image pipe1 = new ImageIcon("src/main/resources/pictures/pipe.png").getImage();
+    private static final Image pipe2 = new ImageIcon("src/main/resources/pictures/pipe2.png").getImage();
 
     java.util.Timer timer;
-
-    public List<PipeView> pipes;
 
     public String name;
     private Point clickPoint;
@@ -31,7 +28,7 @@ public class NewGame extends JPanel {
     GameConfiguration configuration;
     Window parent;
 
-    List<GameObject> gameState;
+    List<GameObject> gameState = new ArrayList<>();
 
     private void addEventListeners() {
         this.addMouseListener(new MouseAdapter() {
@@ -44,10 +41,6 @@ public class NewGame extends JPanel {
         });
     }
 
-    private void birdMove(){
-        gamePresenter.birdMoveX();
-    }
-
     public NewGame(GamePresenter presenter, Window window, GameConfiguration config) {
         gamePresenter = presenter;
         configuration = config;
@@ -57,78 +50,51 @@ public class NewGame extends JPanel {
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
+                gamePresenter.update();
                 repaint();
             }
         };
         timer.schedule(timerTask, 0, 20);
 
-        bird = new ImageIcon("src/main/java/Pictures/bird7.png").getImage();
-        back = new ImageIcon("src/main/java/Pictures/back7.png").getImage();
-        pipe1 = new ImageIcon("src/main/java/Pictures/pipe.png").getImage();
-        pipe2 = new ImageIcon("src/main/java/Pictures/pipe2.png").getImage();
-
         addEventListeners();
     }
 
-    public void crashedBird(Graphics g) {
-        pipes = gamePresenter.getPipes();
-        for (PipeView p : pipes) {
-            if (p.XPipe() - gamePresenter.BirdCoordX() < configuration.field_width){
-                g.drawImage(pipe1, p.XPipe() - gamePresenter.BirdCoordX(),
-                        p.YBottomPipe(), configuration.pipe_width, configuration.pipe_height, this);
-                g.drawImage(pipe2, p.XPipe() - gamePresenter.BirdCoordX(),
-                        p.YTopPipe(), configuration.pipe_width, configuration.pipe_height, this);
-
-                if (((p.YBottomPipe() - gamePresenter.BirdCoordY() < configuration.bird_height) &&
-                        Math.abs(p.XPipe() - (gamePresenter.BirdCoordX() + configuration.bird_startX)) < configuration.bird_width)
-                    || ((p.YBottomPipe() - configuration.distance_height_pipes > gamePresenter.BirdCoordY()) &&
-                        Math.abs(p.XPipe() - (gamePresenter.BirdCoordX() + configuration.bird_startX)) < configuration.bird_width)) {
-                    gameOver();
-                } else if (gamePresenter.BirdCoordX() + configuration.bird_startX  > configuration.pipe_startX + configuration.pipe_num*configuration.distance_width_pipes) {
-                    gameOver();
-                } else if (p.XPipe() - (gamePresenter.BirdCoordX() + configuration.bird_startX) < configuration.bird_width){
-                    p.setPassed();
-                }
+    public void drawPipes(Graphics g) {
+        for (int i = 1; i < gameState.size(); i++){
+            GameObject.PipeModel current = (GameObject.PipeModel) gameState.get(i);
+            if (current.x() - gamePresenter.BirdCoordX() < configuration.field_width){
+                g.drawImage(pipe1, current.x() - gamePresenter.BirdCoordX(),
+                        current.yBottom(), configuration.pipe_width, configuration.pipe_height, this);
+                g.drawImage(pipe2, current.x() - gamePresenter.BirdCoordX(),
+                        current.yTop(), configuration.pipe_width, configuration.pipe_height, this);
             }
         }
     }
 
-    //|| ((p.YTopPipe() - gamePresenter.BirdCoordY() < configuration.bird_height/2) &&
-      //      Math.abs(p.XPipe() - (gamePresenter.BirdCoordX() + 300 )) < configuration.bird_width/2)
-    //|| ((gamePresenter.BirdCoordY() - p.YTopPipe() < configuration.bird_height) &&
-      //      Math.abs(p.XPipe() - (gamePresenter.BirdCoordX() + 300 )) < configuration.bird_width))
 
-    private void gameOver(){
-        gamePresenter.updateScore();
-        gamePresenter.birdDeath();
+    public void gameOver(){
+        gamePresenter.gameOver();
         timer.cancel();
         parent.dispose();
     }
 
-    public boolean birdInsideField(int y){
-        return y < configuration.field_height && y > 0;
-    }
-
     public void drawBird(Graphics g) {
         int x = configuration.bird_startX;
-        int y = gamePresenter.BirdCoordY();
-        if (birdInsideField(y)){
-            g.drawImage(bird, x, y, configuration.bird_width, configuration.bird_height, this);
-        } else {
-            gameOver();
+        if (gameState.size() > 0) {
+            GameObject.BirdModel birdObject = (GameObject.BirdModel) gameState.get(0);
+            g.drawImage(bird, x, birdObject.y(), configuration.bird_width, configuration.bird_height, this);
         }
     }
 
     @Override
     public void paintComponent (Graphics g) {
         super.paintComponent(g);
-        //g.drawImage(back, -1 * (gamePresenter.BirdCoordX() % gamePresenter.BirdCoordX()), 0, 2700, 790, this);
-        g.drawImage(back, 0, 0, 2700, 790, this);
-
-        crashedBird(g);
-        birdMove();
-        GamePresenter.usualBirdAction();
+        g.drawImage(back, configuration.back_x, configuration.back_y, configuration.back_width, configuration.back_height, this);
         drawBird(g);
+        drawPipes(g);
     }
 
+    public void setGameState(List<GameObject> obj) {
+        gameState = obj;
+    }
 }
