38f45f916840638bcc7153fc3e70f9b384113b0f
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-28 19:21:10 +0700

    Added Pipe

--- src/main/java/model/Pipe.java
+++ src/main/java/model/Pipe.java
@@ -3,23 +3,31 @@ package model;
 import java.util.Random;
 
 public class Pipe {
-    private static final int HEIGHT_PIPE = 1000;
-
-    private static final int DISTANCE = 300;
-    private static final int DISTANCE_HEIGHT = HEIGHT_PIPE + DISTANCE;
-
-    private static final int min_location_pipe = 200;
-    private static final int max_location_pipe = 700;
+    private static int heightPipe;
+    private static int distance;
+    private int distanceHeight = heightPipe + distance;
+    private static int min_location_pipe;
+    private static int max_location_pipe;
 
     private int yBottom;
     private int yTop;
     public int x;
+    private boolean passed = false;
+
+    public Pipe(int heightPipe, int distance, int min_location_pipe, int max_location_pipe) {
+        this.heightPipe = heightPipe;
+        this.distance = distance;
+        this.min_location_pipe = min_location_pipe;
+        this.max_location_pipe = max_location_pipe;
+    }
 
     public Pipe(int x) {
         this.x = x;
         Random rand = new Random();
-        yBottom = rand.nextInt(max_location_pipe - min_location_pipe) + min_location_pipe;
-        yTop = yBottom - DISTANCE_HEIGHT;
+        if (max_location_pipe > min_location_pipe) {
+            yBottom = rand.nextInt(max_location_pipe - min_location_pipe) + min_location_pipe;
+            yTop = yBottom - distanceHeight;
+        }
     }
 
     public int x() {
@@ -32,5 +40,13 @@ public class Pipe {
 
     public int yTop() {
         return yTop;
+    }
+
+    public boolean IsPassed(){
+        return passed;
+    }
+
+    public void setPassed() {
+        passed = true;
     }
 }
