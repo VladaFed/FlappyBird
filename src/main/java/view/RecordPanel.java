43bf7bd21802e835e8044378d4cae326c0dfdc41
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-28 19:24:46 +0700

    Update RecordPanel.java

--- src/main/java/view/RecordPanel.java
+++ src/main/java/view/RecordPanel.java
@@ -4,20 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class RecordPanel extends JFrame {
+    private static final String[] columnNames = {"Name", "Score"};
     JFrame frameTable = new JFrame("Table of record");
 
-    public RecordPanel() {}
-
-    public void tableGame(String[][] data, String[] columnNames) {
+    public void tableGame(String[][] data, int panelWidth, int panelHeight) {
         JTable table = new JTable(data, columnNames);
         JScrollPane scrollPane = new JScrollPane(table);
-
         frameTable.getContentPane().add(scrollPane);
-        frameTable.setPreferredSize(new Dimension(450, 200));
+        frameTable.setPreferredSize(new Dimension(panelWidth, panelHeight));
         frameTable.pack();
         frameTable.setLocationRelativeTo(null);
         frameTable.setVisible(true);
     }
-
 
 }
