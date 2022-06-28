65a3b73a859a3a959bff6ac7637ba12a898688d7
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-28 19:15:47 +0700

    Added gameConfigurationMenu

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,9 +7,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         GameConfiguration gameConfiguration = new GameConfiguration("config.properties");
+        GameConfigurationMenu gameConfigurationMenu = new GameConfigurationMenu("menu.properties");
         gameConfiguration.readFromProperties();
+        gameConfigurationMenu.readFromProperties();
 
-        MainFrame frame = new MainFrame(gameConfiguration);
+
+        MainFrame frame = new MainFrame(gameConfigurationMenu, gameConfiguration);
         //FieldPanel fieldPanel = new FieldPanel(gameConfiguration);
     }
 }
