d61970912dce126d9c00a50d676ea3186146dc30
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-28 19:23:10 +0700

    Update About.java

--- src/main/java/view/About.java
+++ src/main/java/view/About.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class About extends JPanel {
     public void paintComponent (Graphics g) {
-        Image image = new ImageIcon("src/main/java/Pictures/about.png").getImage();
+        Image image = new ImageIcon("src/main/resources/pictures/about.png").getImage();
         g.drawImage(image, 0, 0, this);
     }
 }
