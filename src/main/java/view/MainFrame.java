c4077f916b6001fd29dac7022cc4d894f68e0ba6
VladaFed <96881646+VladaFed@users.noreply.github.com>
2022-06-29 19:39:02 +0700

    Changed names

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -1,140 +1,137 @@
 package view;
 
 import controller.GamePresenter;
-import jdk.jfr.consumer.RecordedFrame;
 import main.GameConfiguration;
 import main.GameConfigurationMenu;
-import model.Record;
-import model.TableRecord;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.List;
 
 public class MainFrame extends JFrame implements ActionListener {
-    private JLabel titleL, nameL, markL, warningL;
-    private JButton registrationB, aboutB, exitB, recordB, startGameB;
-    public JTextField nameT = new JTextField(15);
+    private JLabel nameGameLabel, nameLabel, markLabel, warningLabel;
+    private JButton registrationButton, aboutButton, exitButton, recordButton, startGameButton;
+    public JTextField nameTextField = new JTextField(15);
     public String name;
     private final GameConfiguration gameConfiguration;
     private final GameConfigurationMenu gameConfigurationMenu;
 
     GamePresenter gamePresenter;
 
-    JFrame frame1 = new JFrame("Menu");
-    JFrame frame2 = new JFrame("Registration");
-    JFrame about = new JFrame("About");
+    JFrame frameMenu = new JFrame("Menu");
+    JFrame frameRegistration = new JFrame("Registration");
+    JFrame frameAbout = new JFrame("About");
 
     public MainFrame(GameConfigurationMenu gameConfigurationMenu, GameConfiguration gameConfiguration) {
         super("FlappyBird");
         this.gameConfigurationMenu = gameConfigurationMenu;
         this.gameConfiguration = gameConfiguration;
-        frame1.setSize(gameConfigurationMenu.menu_width, gameConfigurationMenu.menu_height);
-        frame1.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frameMenu.setSize(gameConfigurationMenu.menuWidth, gameConfigurationMenu.menuHeight);
+        frameMenu.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        Container mainP = frame1.getContentPane();
-        mainP.setLayout(null);
+        Container mainPanel = frameMenu.getContentPane();
+        mainPanel.setLayout(null);
 
-        titleL = new JLabel("Flappy Bird");
-        warningL = new JLabel("The high score table is available after the first game");
-        registrationB = new JButton("Registration");
-        aboutB = new JButton("About");
-        recordB = new JButton("Table of record");
-        exitB = new JButton("Exit");
+        nameGameLabel = new JLabel("Flappy Bird");
+        warningLabel = new JLabel("The high score table is available after the first game");
+        registrationButton = new JButton("Registration");
+        aboutButton = new JButton("About");
+        recordButton = new JButton("Table of record");
+        exitButton = new JButton("Exit");
 
 
-        mainP.add(titleL);
-        titleL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.flappy_bird_size));
-        titleL.setBounds(gameConfigurationMenu.flappy_bird_x, gameConfigurationMenu.flappy_bird_y,
-                gameConfigurationMenu.flappy_bird_width, gameConfigurationMenu.flappy_bird_height);
+        mainPanel.add(nameGameLabel);
+        nameGameLabel.setFont(new Font("TimesRoman", Font.BOLD, gameConfigurationMenu.flappyBirdSize));
+        nameGameLabel.setBounds(gameConfigurationMenu.flappyBirdX, gameConfigurationMenu.flappyBirdY,
+                gameConfigurationMenu.flappyBirdWidth, gameConfigurationMenu.flappyBirdHeight);
 
-        mainP.add(warningL);
-        warningL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.warning_bird_size));
-        warningL.setBounds(gameConfigurationMenu.warning_x, gameConfigurationMenu.warning_y,
-                gameConfigurationMenu.warning_width, gameConfigurationMenu.warning_height);
+        mainPanel.add(warningLabel);
+        warningLabel.setFont(new Font("TimesRoman", Font.BOLD, gameConfigurationMenu.warningBirdSize));
+        warningLabel.setBounds(gameConfigurationMenu.warningX, gameConfigurationMenu.warningY,
+                gameConfigurationMenu.warningWidth, gameConfigurationMenu.warningHeight);
 
-        mainP.add(registrationB);
-        registrationB.setBounds(gameConfigurationMenu.registration_x, gameConfigurationMenu.registration_y,
-                gameConfigurationMenu.registration_width, gameConfigurationMenu.registration_height);
-        registrationB.addActionListener(event -> panelStart());
+        mainPanel.add(registrationButton);
+        registrationButton.setBounds(gameConfigurationMenu.registrationX, gameConfigurationMenu.registrationY,
+                gameConfigurationMenu.registrationWidth, gameConfigurationMenu.registrationHeight);
+        registrationButton.addActionListener(event -> panelStart());
 
-        mainP.add(aboutB);
-        aboutB.setBounds(gameConfigurationMenu.about_x, gameConfigurationMenu.about_y,
-                gameConfigurationMenu.about_width, gameConfigurationMenu.about_height);
-        aboutB.addActionListener(event -> printAbout());
+        mainPanel.add(aboutButton);
+        aboutButton.setBounds(gameConfigurationMenu.aboutX, gameConfigurationMenu.aboutY,
+                gameConfigurationMenu.aboutWidth, gameConfigurationMenu.aboutHeight);
+        aboutButton.addActionListener(event -> printAbout());
 
-        mainP.add(recordB);
-        recordB.setBounds(gameConfigurationMenu.record_x, gameConfigurationMenu.record_y,
-                gameConfigurationMenu.record_width, gameConfigurationMenu.record_height);
-        recordB.addActionListener(event -> tableGame());
+        mainPanel.add(recordButton);
+        recordButton.setBounds(gameConfigurationMenu.recordX, gameConfigurationMenu.recordY,
+                gameConfigurationMenu.recordWidth, gameConfigurationMenu.recordHeight);
+        recordButton.addActionListener(event -> tableGame());
 
-        mainP.add(exitB);
-        exitB.setBounds(gameConfigurationMenu.exit_x, gameConfigurationMenu.exit_y,
-                gameConfigurationMenu.exit_width, gameConfigurationMenu.exit_height);
-        exitB.addActionListener(event -> System.exit(0));
+        mainPanel.add(exitButton);
+        exitButton.setBounds(gameConfigurationMenu.exitX, gameConfigurationMenu.exitY,
+                gameConfigurationMenu.exitWidth, gameConfigurationMenu.exitHeight);
+        exitButton.addActionListener(event -> System.exit(0));
 
-        frame1.setLocationRelativeTo(null);
-        frame1.setVisible(true);
-        frame1.setResizable(false);
+        frameMenu.setLocationRelativeTo(null);
+        frameMenu.setVisible(true);
+        frameMenu.setResizable(false);
 
     }
 
     public void panelStart() {
-        frame2.setSize(gameConfigurationMenu.panel_start_width, gameConfigurationMenu.panel_start_height);
-        frame2.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frameRegistration.setSize(gameConfigurationMenu.panelStartWidth, gameConfigurationMenu.panelStartHeight);
+        frameRegistration.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        Container startP = frame2.getContentPane();
+        Container startP = frameRegistration.getContentPane();
         startP.setLayout(null);
 
-        nameL = new JLabel("Registration");
-        markL = new JLabel("Enter name: ");
-        startGameB = new JButton("Start");
+        nameLabel = new JLabel("Registration");
+        markLabel = new JLabel("Enter name: ");
+        startGameButton = new JButton("Start");
 
-        startP.add(nameL);
-        nameL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.registration_size));
-        nameL.setBounds(gameConfigurationMenu.registration_label_x, gameConfigurationMenu.registration_label_y,
-                gameConfigurationMenu.registration_label_width, gameConfigurationMenu.registration_label_height);
+        startP.add(nameLabel);
+        nameLabel.setFont(new Font("TimesRoman", Font.BOLD, gameConfigurationMenu.registrationSize));
+        nameLabel.setBounds(gameConfigurationMenu.registrationLabelX, gameConfigurationMenu.registrationLabelY,
+                gameConfigurationMenu.registrationLabelWidth, gameConfigurationMenu.registrationLabelHeight);
 
-        startP.add(markL);
-        markL.setFont(new Font("Chiller", Font.ITALIC, gameConfigurationMenu.enter_name_size));
-        markL.setBounds(gameConfigurationMenu.enter_name_x, gameConfigurationMenu.enter_name_y,
-                gameConfigurationMenu.enter_name_width, gameConfigurationMenu.enter_name_height);
+        startP.add(markLabel);
+        markLabel.setFont(new Font("TimesRoman", Font.BOLD, gameConfigurationMenu.enterNameSize));
+        markLabel.setBounds(gameConfigurationMenu.enterNameX, gameConfigurationMenu.enterNameY,
+                gameConfigurationMenu.enterNameWidth, gameConfigurationMenu.enterNameHeight);
 
-        startP.add(startGameB);
-        startGameB.setBounds(gameConfigurationMenu.start_x, gameConfigurationMenu.start_y,
-                gameConfigurationMenu.start_width, gameConfigurationMenu.start_height);
-        startGameB.addActionListener(event -> {
+        startP.add(startGameButton);
+        startGameButton.setBounds(gameConfigurationMenu.startX, gameConfigurationMenu.startY,
+                gameConfigurationMenu.startWidth, gameConfigurationMenu.startHeight);
+        startGameButton.addActionListener(event -> {
             try {
-                frame2.dispose();
-                name = nameT.getText();
+                frameRegistration.dispose();
+                name = nameTextField.getText();
                 if (name.length() > 15 || !isValid(name)) return;
-                gamePresenter = new GamePresenter(gameConfiguration);
+                gamePresenter = new GamePresenter(gameConfiguration, gameConfigurationMenu);
                 gamePresenter.newGame(name);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
-        startP.add(nameT);
-        nameT.setBounds(gameConfigurationMenu.name_x, gameConfigurationMenu.name_y,
-                gameConfigurationMenu.name_width, gameConfigurationMenu.name_height);
-        nameT.addActionListener(this);
+        startP.add(nameTextField);
+        nameTextField.setBounds(gameConfigurationMenu.nameX, gameConfigurationMenu.nameY,
+                gameConfigurationMenu.nameWidth, gameConfigurationMenu.nameHeight);
+        nameTextField.addActionListener(this);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame2.setLocationRelativeTo(null);
-        frame2.setVisible(true);
-        frame2.setResizable(false);
+        frameRegistration.setLocationRelativeTo(null);
+        frameRegistration.setVisible(true);
+        frameRegistration.setResizable(false);
     }
 
     public void printAbout() {
         About panel = new About();
-        about.getContentPane().add(panel);
-        about.setSize(gameConfigurationMenu.about_panel_width, gameConfigurationMenu.about_panel_height);
-        about.setLocationRelativeTo(null);
-        about.setVisible(true);
+        frameAbout.getContentPane().add(panel);
+        frameAbout.add(panel);
+        frameAbout.setSize(gameConfigurationMenu.aboutPanelWidth, gameConfigurationMenu.aboutPanelHeight);
+        frameAbout.setLocationRelativeTo(null);
+        frameAbout.setVisible(true);
 
     }
 
